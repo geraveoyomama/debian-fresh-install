c467b0cedf48e243e01b2798f1ab2cd60fb12d0f
geraveoyomama <77015630+geraveoyomama@users.noreply.github.com>
2024-09-29 16:53:32 +0200

    Initial debian-fresh commit
    expecting a sid environment.

---
+++ debian-desktop.sh
@@ -0,0 +1,8 @@
+sudo apt install htop lm-sensors curl wget sudo pip linux-image-amd64 linux-headers-amd64 linux-cpupower wine winetricks firewalld nvtop amd64-microcode dnsmasq resolvconf \
+    ckb-next nextcloud-desktop dolphin-nextcloud yakuake keepassxc-full spyder firefox firefox-esr- konsole \
+    sddm plasma-workspace-wayland gamemode firefox firefox-esr- plasma-discover-backend-flatpak kde-config-flatpak \
+    wireplumber pipewire-pulse pipewire-alsa pipewire-media-session- libspa-0.2-bluetooth pulseaudio-module-bluetooth- libspa-0.2-libcamera
+
+systemctl --user --now enable wireplumber.service
+systemctl --user restart wireplumber pipewire pipewire-pulse
+
