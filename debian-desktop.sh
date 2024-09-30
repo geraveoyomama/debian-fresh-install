949c6ec56f24803a76744ef251112dc9394c2d33
geraveoyomama <77015630+geraveoyomama@users.noreply.github.com>
2024-09-30 15:49:27 +0200

    Update debian-desktop.sh

--- debian-desktop.sh
+++ debian-desktop.sh
@@ -1,5 +1,5 @@
 sudo apt install -y htop lm-sensors curl wget sudo pip linux-image-amd64 linux-headers-amd64 linux-cpupower wine winetricks firewalld nvtop amd64-microcode dnsmasq resolvconf \
-    ckb-next nextcloud-desktop dolphin-nextcloud yakuake keepassxc-full spyder firefox firefox-esr- konsole virt-manager \
+    ckb-next nextcloud-desktop dolphin-nextcloud yakuake keepassxc-full spyder firefox firefox-esr- konsole virt-manager spectacle \
     sddm plasma-workspace-wayland gamemode firefox firefox-esr- plasma-discover-backend-flatpak kde-config-flatpak \
     wireplumber pipewire-pulse pipewire-alsa pipewire-media-session- libspa-0.2-bluetooth pulseaudio-module-bluetooth- libspa-0.2-libcamera
 
