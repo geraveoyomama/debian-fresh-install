47ff418c28ebf0055a53898f27b76a2880c2ba28
geraveoyomama <77015630+geraveoyomama@users.noreply.github.com>
2024-10-14 13:29:03 +0200

    Update podman-systemd.sh

--- podman-systemd.sh
+++ podman-systemd.sh
@@ -1,1 +1,4 @@
+mkdir -p ~/.config/systemd/user/
 podman generate systemd --new --name $1 > ~/.config/systemd/user/container-$1.service
+systemctl --user daemon-reload
+systemctl --user status container-$1.service
