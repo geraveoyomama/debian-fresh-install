d61c87c737edab57a806d49a95d42efc4cccd5b8
geraveoyomama <77015630+geraveoyomama@users.noreply.github.com>
2025-02-26 22:03:34 +0100

    Update debian-desktop.sh

--- debian-desktop.sh
+++ debian-desktop.sh
@@ -4,7 +4,7 @@ sudo apt install -y htop lm-sensors curl wget sudo pip linux-image-amd64 linux-h
     ckb-next nextcloud-desktop dolphin-nextcloud yakuake keepassxc-full spyder firefox firefox-esr- konsole virt-manager kde-spectacle ark \
     sddm plasma-desktop gamemode firefox firefox-esr- plasma-discover-backend-flatpak kde-config-flatpak \
     wireplumber pipewire-pulse pipewire-alsa pipewire-media-session- libspa-0.2-bluetooth pulseaudio-module-bluetooth- libspa-0.2-libcamera
-sudo apt purge tasksel* wamerican ispell emacsen-common doc-debian debian-faq
+sudo apt purge tasksel* wamerican ispell hunspell-en-us emacsen-common doc-debian debian-faq
 
 systemctl --user --now enable wireplumber.service
 systemctl --user restart wireplumber pipewire pipewire-pulse
